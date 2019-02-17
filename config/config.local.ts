65c6cfec96e26006f6cd2dc671d531dcfab5c9d3
luckyscript <jsjhlk@qq.com>
2019-02-17 22:11:07 +0800

    toc 样式

--- config/config.local.ts
+++ config/config.local.ts
@@ -2,5 +2,14 @@ import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
+  config.sequelize = {
+    dialect: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'nbook',
+    timestamps: false,
+  };
   return config;
 };

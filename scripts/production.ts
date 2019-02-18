c7e3f74056780a636892a9cfe1e863229478a008
luckyscript <main.lukai@gmail.com>
2019-02-18 19:37:04 +0800

    build css script

---
+++ scripts/production.ts
@@ -0,0 +1,31 @@
+import { readFile, writeFileSync } from 'fs';
+import { resolve } from 'path';
+
+const build = () => {
+  const plugins: any[] = [
+    require('postcss-easy-import'),
+    require('postcss-cssnext'),
+    require('cssnano')({
+      autoprefixer: false,
+      zindex: false,
+      preset: [ 'default', {
+        discardComments: {
+          removeAll: true,
+        },
+      }],
+    }),
+  ];
+  const src = resolve(__dirname, '..', 'app', 'public', 'pcss', 'common.pcss');
+  const desc = resolve(__dirname, '..', 'app', 'public', 'stylesheets', 'common.css');
+  readFile(src, (err, css) => {
+    if (err) {
+      throw new Error(`${err}`);
+    }
+    const postcss = require('postcss');
+    postcss(plugins).process(css, { from: src, to: desc }).then(data => {
+      writeFileSync(desc, data.css);
+    });
+  });
+};
+
+build();

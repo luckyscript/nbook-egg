a9148ce95cb05c454cc3daf1251df9b392205312
luckyscript <main.lukai@gmail.com>
2019-03-29 14:25:12 +0800

    feature:tag&category curd

--- app/controller/api/category.ts
+++ app/controller/api/category.ts
@@ -12,6 +12,20 @@ class CategoryController extends Controller {
     const resultDTO = await ctx.service.category.findCategoryTree();
     return ctx.body = ctx.success(resultDTO);
   }
+
+  async addCategory() {
+    const { ctx } = this;
+    const { id, pid, name } = ctx.request.body;
+    const moment = require('moment');
+    const created = moment().format('YYYY-MM-DD HH:mm:ss');
+    await ctx.model.Category.upsert({
+      id,
+      pid,
+      name,
+      created,
+    });
+    ctx.body = ctx.success('创建成功');
+  }
 }
 
 export default CategoryController;
--- app/controller/api/tag.ts
+++ app/controller/api/tag.ts
@@ -6,6 +6,16 @@ class TagController extends Controller {
     const resultDTO = await ctx.model.Tag.findAll();
     ctx.body = ctx.success(resultDTO);
   }
+
+  async addTag() {
+    const { ctx } = this;
+    const { id, name } = ctx.request.body;
+    await ctx.model.Tag.upsert({
+      id,
+      name,
+    });
+    ctx.body = ctx.success('创建成功');
+  }
 }
 
 export default TagController;

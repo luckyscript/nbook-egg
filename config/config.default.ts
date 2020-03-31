fe4f4b847de665067fa20efaecdbe6f4770d9d69
luckyscript <main.lukai@gmail.com>
2020-03-31 15:34:50 +0800

    fix db

--- config/config.default.ts
+++ config/config.default.ts
@@ -45,7 +45,7 @@ export default (appInfo: EggAppInfo) => {
     port: 3306,
     user: 'root',
     password: 'lukai123',
-    database: 'nbook',
+    database: 'nbook_egg',
     timestamps: false,
   };
 
